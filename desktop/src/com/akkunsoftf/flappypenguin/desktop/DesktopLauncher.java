18369c140546c5edaaf841e70d9f7ec7fa70e83e
Akkun <paquet.alex@gmail.com>
2017-03-03 18:37:40 -0500

    libGDX project created

---
+++ desktop/src/com/akkunsoftf/flappypenguin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.akkunsoftf.flappypenguin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.akkunsoftf.flappypenguin.FlappyPenguin;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyPenguin(), config);
+	}
+}
