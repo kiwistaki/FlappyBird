33ea71b997513b56cfd26c39037782e1d33510b6
Akkun <paquet.alex@gmail.com>
2017-03-03 19:01:50 -0500

    First Commit

---
+++ desktop/src/com/akkunsoft/flappypenguin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.akkunsoft.flappypenguin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.akkunsoft.flappypenguin.FlappyPenguin;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyPenguin(), config);
+	}
+}
